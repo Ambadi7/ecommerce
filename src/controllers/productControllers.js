2a32498507aacde6ec92b1a7894ce27f9fdff995
Ambadi7 <ambadikr24@gmail.com>
2025-06-18 10:56:50 +0530

    product-crud-complete

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -28,7 +28,7 @@ export const createProduct = async(req ,res) =>{
         // if there is photo , we will make some changes in the product we receive
         //sinc the date through fs module  and pass the path
 
-                if (photo) {
+        if (photo) {
             product.photo.data = fs.readFileSync(photo.path)
             product.photo.contentType = photo.type
         }
@@ -65,6 +65,131 @@ export const getAllProducts = async (req,res) =>{
         res.status(500).json({
             success : false ,
             message : `Error in get all products ${error}`,
+            error
+        })
+    }
+}
+
+//get single product controller
+
+export const getSingleProduct = async (req,res) =>{
+    try{
+        //destructuring another method
+        const product = await Product.findOne({slug :req.params.slug}).select("-photo").populate("collection")
+
+        res.status(200).json({
+            success : true,
+            message : `Successfully fetch the single collection`,
+            product
+        })
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({
+            success : false ,
+            message : `Error in fetching single product ${error}`,
+            error
+        })
+    }
+}
+
+//fetch photo
+export const productPhoto = async (req,res) =>{
+    try{
+        const product = await Product.findById(req.params.pid).select("photo")
+
+        if(product.photo.data){
+            res.set("Content-type",product.photo.contentType)
+            return res.status(200).send(product.photo.data)
+        }
+    }catch(error){
+        console.log(error)
+        res.status(500).json({
+            success : false,
+            message : `Error in fetching photo ${error}`,
+            error
+        })
+    }
+}
+
+
+export const deleteProduct = async (req,res) => {
+    try{
+
+        const alreadyDeleteProduct = await Product.findById(req.params.pid).select("-photo")
+
+        if(!alreadyDeleteProduct){
+            return res.status(200).json({
+                success : false,
+                message : "NO product found"
+            })
+        }
+
+        await Product.findByIdAndDelete(req.params.pid).select("-photo")
+
+
+        res.status(200).json({
+            success : true,
+            message : "Product has been deleted successfully"
+        })
+    }catch(error){
+        console.log(error)
+        res.status(500).json({
+            success : false,
+            message :`Error in delete product ${error}`,
+            error
+        })
+    }
+}
+
+//product update
+
+export const updateProduct = async (req,res) =>{
+    try{
+        const {name , description , price , collection , quantity , shipping} = req.fields
+        const { photo } = req.files
+
+        //fields validation
+        if(!name || !description || !price || !collection || !quantity || !shipping){
+            return res.status(400).json({
+                success : false ,
+                message : "fIll all the fields"
+            })
+        }
+
+        //photo validation
+        if(!photo &&photo.size>1000000){
+            res.status(400).json({
+                success : false,
+                message : "photo is required and should be less than 1MB"
+            })
+        }
+
+        //update
+        const product = await Product.findByIdAndUpdate(
+            req.params.pid,
+            {...req.fields,slug:slugify(name)},
+            {new:true}
+        )
+
+        //update photo
+        if(photo){
+            product.photo.data = fs.readFileSync(photo.path);
+            product.photo.contentType = photo.type
+        }
+
+        await product.save()
+        res.status(201).json({
+            success : true,
+            message : "Product updated Successfully",
+            product
+        })
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({
+            success : false ,
+            message :`Error in update product ${error}`,
             error
         })
     }
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,10 +1,14 @@
 import formidable from "express-formidable";
 import express from 'express'
-import { createProduct, getAllProducts } from "../controllers/productControllers.js";
+import { createProduct, deleteProduct, getAllProducts, getSingleProduct, productPhoto, updateProduct } from "../controllers/productControllers.js";
+import { isLoggedIn } from "../middlewares/authMiddlewares.js";
 
 const router = express.Router()
 
 router.post("/create-product", formidable(),createProduct)
 router.get("/get-allproducts",getAllProducts)
-
+router.get("/single-product/:slug",isLoggedIn,getSingleProduct)
+router.get("/product-photo/:pid",productPhoto)
+router.delete("/product-delete/:pid",isLoggedIn,deleteProduct)
+router.put("/update-product/:pid",formidable(),updateProduct)
 export default router
